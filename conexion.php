617558c8cafb22510468a48567dc2d6e38e8367d
Rodrigo Barrera <bar20807@uvg.edu.gt>
2023-11-19 16:29:25 -0600

    crud camcor

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$servidor = "localhost";
+$db ="products";
+$username = "root";
+$password = "";
+
+try {
+    $conexion = new PDO("mysql:host=$servidor; dbname = $db", $username, $password);
+
+} catch (Exception $e) {
+    echo $e -> getMessage();    
+}
+
+
+
+
+?>
